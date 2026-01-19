ccdb304284d9b794054dcd19df41500c3bf25d74
giftclubgiftclub-2026 <giftclub.giftclub@gmail.com>
2026-01-19 16:27:55 +0100

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.QRTP4_CONFIG = {
   SUPABASE_URL: "https://aaczdhxzzsvrvwbfgvst.supabase.co",
-  SUPABASE_ANON_KEY: "ВСТАВЬ_СЮДА_ANON_KEY_ИЗ_SUPABASE",
-  PASSPORTS_TABLE: "passports"
+    SUPABASE_ANON_KEY: "sb_publishable_7yerv2q_tfX3lVO6EA-Y1g_pyg0ufEg",
+      PASSPORTS_TABLE: "passports"
 };
